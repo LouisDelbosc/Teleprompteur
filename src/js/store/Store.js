4d1da1d1d4ba40367b2851980ab511bd428c0ea8
LouisDelbosc <louisdelbosc@gmail.com>
2015-09-04 15:26:37 +0200

    add font size in store

--- src/js/store/Store.js
+++ src/js/store/Store.js
@@ -1,16 +1,29 @@
 var Store = {
 
   state: {
-    text: ''
+    text: '',
+    fontSize: 50
   },
 
   saveText(text) {
     this.state.text = text;
   },
 
+  saveFontSize(fontSize) {
+    this.state.fontSize = fontSize;
+    console.log('save fs');
+    this.onChange();
+  },
+
   getText() {
     return this.state.text;
-  }
+  },
+
+  getFontSize() {
+    return this.state.fontSize;
+  },
+
+  onChange() {}
 
 };
 
